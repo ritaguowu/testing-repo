565a4c20967bc425d1a0be756fec8bf78894f67b
rita <ritaguowu@email.com>
2018-04-10 15:10:22 -0400

    about5

--- src/test/java/com/ecvictor/testng/nebula/module/AboutPage.java
+++ src/test/java/com/ecvictor/testng/nebula/module/AboutPage.java
@@ -3,15 +3,15 @@ package com.ecvictor.testng.nebula.module;
 import com.ecvictor.testng.nebula.BaseTest;
 import com.ecvictor.testng.nebula.pageObject.Aboutpage;
 import com.ecvictor.testng.nebula.pageObject.Homepage;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.CoreMatchers.containsString;
@@ -86,31 +86,37 @@ public  class AboutPage extends BaseTest {
 
     }
 
-    public static void clickVedio(WebDriver driver) throws InterruptedException {
+    public static void clickVedio(WebDriver driver) throws IOException {
 
-        System.out.println("Start ...ksfdlkjfljsdlfjldsf");
+        try {
+            System.out.println("Start ...ksfdlkjfljsdlfjldsf");
 
-        driver.switchTo().frame(Aboutpage.IFrame);
+            driver.switchTo().frame(Aboutpage.IFrame);
 
-        System.out.println("after frame ...ksfdlkjfljsdlfjldsf");
-        Aboutpage.StartPlayVideo.click(); // click some link that opens a new window
+            System.out.println("after frame ...ksfdlkjfljsdlfjldsf");
+            Aboutpage.StartPlayVideo.click(); // click some link that opens a new window
 
-        Thread.sleep(5000);
+            Thread.sleep(5000);
 
-        System.out.println("before sdlkfjsdjfljsdg");
-        Aboutpage.PlayButton.click();
-        Thread.sleep(2000);
+            System.out.println("before sdlkfjsdjfljsdg");
+            Aboutpage.PlayButton.click();
+            Thread.sleep(2000);
 
-        System.out.println("after sdkgjldjgljsdlafj");
+            System.out.println("after sdkgjldjgljsdlafj");
 
-        String playtime = Aboutpage.VideoTime.getAttribute("innerHTML");
+            String playtime = Aboutpage.VideoTime.getAttribute("innerHTML");
 
-        System.out.println("time:"+playtime);
+            System.out.println("time:" + playtime);
 
 
-        Assert.assertNotEquals(playtime, "0.00");
+            Assert.assertNotEquals(playtime, "0.00");
 
-        driver.switchTo().defaultContent();
+            driver.switchTo().defaultContent();
+        }catch (Exception){
+            File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+// Now you can do whatever you need to do with it, for example copy somewhere
+            FileUtils.copyFile(scrFile, new File("c:\\tmp\\screenshot.png"));
+        }
 
     }
 
